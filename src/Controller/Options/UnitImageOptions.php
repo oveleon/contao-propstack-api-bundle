c2de7671e863b585939edd747a67e2e73ea73b1a
doishub <daniele@oveleon.de>
2022-06-03 17:20:21 +0200

    Unit image controller prepared

---
+++ src/Controller/Options/UnitImageOptions.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oveleon\ContaoPropstackApiBundle\Controller\Options;
+
+final class UnitImageOptions extends Options
+{
+    protected function configure(): void
+    {
+        $this->set(Options::MODE_UPLOAD, [
+            'photo',
+            'imageable_id',
+            'imageable_type',
+            'is_privat',
+            'title'
+        ]);
+    }
+}
--- src/Controller/Unit/UnitImageController.php
+++ src/Controller/Unit/UnitImageController.php
@@ -2,9 +2,32 @@
 
 namespace Oveleon\ContaoPropstackApiBundle\Controller\Unit;
 
+use Oveleon\ContaoPropstackApiBundle\Controller\Options\Options;
+use Oveleon\ContaoPropstackApiBundle\Controller\Options\UnitImageOptions;
 use Oveleon\ContaoPropstackApiBundle\Controller\PropstackController;
 
+/**
+ * Handle unit image calls
+ *
+ * @link https://docs.propstack.de/reference/objekte/bilder
+ *
+ * @author Daniele Sciannimanica <https://github.com/doishub>
+ */
 class UnitImageController extends PropstackController
 {
+    protected string $route = 'images';
 
+    /**
+     * Upload unit image
+     */
+    public function upload(array $parameters)
+    {
+        $this->call(
+            (new UnitImageOptions(Options::MODE_UPLOAD))
+                ->validate($parameters),
+            self::METHOD_CREATE
+        );
+
+        return $this->getResponse();
+    }
 }
