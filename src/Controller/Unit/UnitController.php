0ff0089bf5eec50cb01d799144aff43a3537f922
doishub <daniele@oveleon.de>
2022-06-10 15:28:23 +0200

    Unit helper function `setRelationship` added

--- src/Controller/Unit/UnitController.php
+++ src/Controller/Unit/UnitController.php
@@ -2,6 +2,7 @@
 
 namespace Oveleon\ContaoPropstackApiBundle\Controller\Unit;
 
+use Oveleon\ContaoPropstackApiBundle\Controller\Options\Constants;
 use Oveleon\ContaoPropstackApiBundle\Controller\Options\Options;
 use Oveleon\ContaoPropstackApiBundle\Controller\Options\UnitOptions;
 use Oveleon\ContaoPropstackApiBundle\Controller\PropstackController;
@@ -16,6 +17,8 @@ use Oveleon\ContaoPropstackApiBundle\Controller\PropstackController;
 class UnitController extends PropstackController
 {
     protected string $route = 'units';
+
+    protected ?array $relations = null;
 
     /**
      * Read units
@@ -47,6 +50,9 @@ class UnitController extends PropstackController
      */
     public function create(array $parameters)
     {
+        // Apply relationships
+        $this->applyRelationships($parameters);
+
         $this->call(
             (new UnitOptions(Options::MODE_CREATE))
                 ->validate(['property' => $parameters]),
@@ -63,6 +69,9 @@ class UnitController extends PropstackController
     {
         // Add id to route
         $this->addRoutePath($id);
+
+        // Apply relationships
+        $this->applyRelationships($parameters);
 
         $this->call(
             (new UnitOptions(Options::MODE_EDIT))
@@ -84,5 +93,48 @@ class UnitController extends PropstackController
         $this->call([], self::METHOD_DELETE);
 
         return $this->getResponse();
+    }
+
+    /**
+     * Add a relation to an existing contact
+     */
+    public function setRelationship(string $relationType, int $relatedClientId, ?array $customData = null): void
+    {
+        $relation = array_merge([
+            'internal_name'     => $relationType,
+            'related_client_id' => $relatedClientId
+        ], $customData ?? []);
+
+        // Only known relations should be added
+        switch($relationType)
+        {
+            case Constants::RELATIONSHIP_OWNER:
+            case Constants::RELATIONSHIP_PARTNER:
+                $this->relations[] = $relation;
+        }
+    }
+
+    /**
+     * Applies the existing relations to the given parameters
+     */
+    private function applyRelationships(&$parameters): void
+    {
+        if(!$this->relations)
+        {
+            return;
+        }
+
+        // Consideration of already given Relationships
+        if(array_key_exists('relationships_attributes', $parameters))
+        {
+            $parameters['relationships_attributes'] = array_merge(
+                $parameters['relationships_attributes'],
+                $this->relations
+            );
+        }
+        else
+        {
+            $parameters['relationships_attributes'] = $this->relations;
+        }
     }
 }
